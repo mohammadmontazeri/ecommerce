0a131f4e9baa698a2ef6ecd5a81c7894965e38f8
mohammadmontazeri <montazeriput95@gmail.com>
2023-12-27 11:03:56 +0330

    add new test in  order_test

--- internal/order/order_test.go
+++ internal/order/order_test.go
@@ -3,11 +3,70 @@ package order
 import (
 	"ecommerce/internal/mocks/ordermocks"
 	"ecommerce/models"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
+
+func TestCreateOrderWhenOrderWithoutProductNotInsert(t *testing.T) {
+	repo := &ordermocks.OrderRepository{}
+
+	repo.On("AddOrderToPivotTable", mock.AnythingOfType("[]int"), mock.AnythingOfType("models.Order")).
+		Return(nil).
+		Once()
+
+	repo.On("InsertOrderWithoutProducts", mock.AnythingOfType("models.Order")).
+		Return(func(db models.Order) models.Order {
+			return db
+		},
+			func(db models.Order) error {
+				var err error = errors.New("new error")
+				return err
+			}).Once()
+
+	orderService := NewOrderService(repo)
+
+	orderInput := models.OrderWithProducts{
+		UserID:     1,
+		Code:       "ppp",
+		Price:      23700,
+		Status:     "s",
+		ProductsID: []int{1},
+	}
+
+	err := orderService.Create(orderInput)
+
+	assert.NotEqual(t, nil, err)
+}
+func TestCreateOrderWhenProductsIDIsNull(t *testing.T) {
+	repo := &ordermocks.OrderRepository{}
+
+	repo.On("AddOrderToPivotTable", mock.AnythingOfType("[]int"), mock.AnythingOfType("models.Order")).
+		Return(nil).
+		Once()
+
+	repo.On("InsertOrderWithoutProducts", mock.AnythingOfType("models.Order")).
+		Return(func(db models.Order) models.Order {
+			return db
+		}, nil).
+		Once()
+
+	orderService := NewOrderService(repo)
+
+	orderInput := models.OrderWithProducts{
+		UserID:     1,
+		Code:       "ppp",
+		Price:      23700,
+		Status:     "s",
+		ProductsID: []int{},
+	}
+
+	err := orderService.Create(orderInput)
+
+	assert.Equal(t, errors.New("error :ProductsID not Corrected !"), err)
+}
 
 func TestCreateOrderService(t *testing.T) {
 
